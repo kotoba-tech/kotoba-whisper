7097efc8c080593c67eda3a16ca4a55f3ea839de
asahi417 <asahi1992ushio@gmail.com>
2024-09-20 11:19:25 +0900

    v3

--- script/distil_whisper_v1.0.sh
+++ script/distil_whisper_v1.0.sh
@@ -125,7 +125,14 @@ do
     python run_short_form_eval.py -m ${MODEL} -d "${DATA}" -b 256
   done
 done
-
+for DATA in "japanese-asr/ja_asr.jsut_basic5000" "japanese-asr/ja_asr.reazonspeech_test" "japanese-asr/ja_asr.common_voice_8_0"
+do
+  python run_short_form_eval.py -l "ja" -t "transcribe" -m "${HF_ORG}/distil-whisper-large-v3-ja-reazonspeech-tiny" -d "${DATA}" -b 256
+  python run_short_form_eval.py -l "ja" -t "transcribe" -m "${HF_ORG}/distil-whisper-large-v3-ja-reazonspeech-small" -d "${DATA}" -b 256
+  python run_short_form_eval.py -l "ja" -t "transcribe" -m "${HF_ORG}/distil-whisper-large-v3-ja-reazonspeech-medium" -d "${DATA}" -b 256
+  python run_short_form_eval.py -l "ja" -t "transcribe" -m "${HF_ORG}/distil-whisper-large-v3-ja-reazonspeech-large" -d "${DATA}" -b 256
+  python run_short_form_eval.py -l "ja" -t "transcribe" -m "${HF_ORG}/distil-whisper-large-v3-ja-reazonspeech-all" -d "${DATA}" -b 256
+done
 ##################################
 # (Optional) Evaluate Nemo Model #
 ##################################
