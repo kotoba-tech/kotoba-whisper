fe047cf2dcfd9cb3026795ce7bb5829d41bea1e4
asahi417 <asahi1992ushio@gmail.com>
2024-09-26 00:56:40 +0900

    add cascade s2t translation

--- misc/cascaded_s2t_translation/push_pipeline.py
+++ misc/cascaded_s2t_translation/push_pipeline.py
@@ -1,22 +1,42 @@
-from ja_cascaded_s2t_translation import CascadedS2TTranslationPipeline
+"""https://github.com/facebookresearch/flores/blob/main/flores200/README.md#languages-in-flores-200
+
+wget https://huggingface.co/datasets/japanese-asr/ja_asr.jsut_basic5000/resolve/main/sample.flac -O sample_ja.flac
+wget https://huggingface.co/datasets/japanese-asr/en_asr.esb_eval/resolve/main/sample.wav -O sample_en.wav
+"""
+from en_cascaded_s2t_translation import EnCascadedS2TTranslationPipeline
+from ja_cascaded_s2t_translation import JaCascadedS2TTranslationPipeline
 from transformers.pipelines import PIPELINE_REGISTRY, pipeline
 from transformers import WhisperForConditionalGeneration, TFWhisperForConditionalGeneration
 
 
-model_alias = "kotoba-tech/kotoba-whisper-v1.1"
 PIPELINE_REGISTRY.register_pipeline(
-    "cascaded-s2t-translation",
-    pipeline_class=CascadedS2TTranslationPipeline,
+    "en-cascaded-s2t-translation",
+    pipeline_class=EnCascadedS2TTranslationPipeline,
     pt_model=WhisperForConditionalGeneration,
     tf_model=TFWhisperForConditionalGeneration
 )
 pipe = pipeline(
-    "cascaded-s2t-translation",
+    "en-cascaded-s2t-translation",
     model="distil-whisper/distil-large-v3",
     model_translation="facebook/nllb-200-distilled-600M",
-    src_lang="eng_Latn",
     tgt_lang="jpn_Jpan",
     chunk_length_s=15,
     device_map="auto"
 )
-pipe.push_to_hub("japanese-asr/cascaded-s2t-translation")
+pipe.push_to_hub("japanese-asr/en-cascaded-s2t-translation")
+
+PIPELINE_REGISTRY.register_pipeline(
+    "ja-cascaded-s2t-translation",
+    pipeline_class=JaCascadedS2TTranslationPipeline,
+    pt_model=WhisperForConditionalGeneration,
+    tf_model=TFWhisperForConditionalGeneration
+)
+pipe = pipeline(
+    "ja-cascaded-s2t-translation",
+    model="kotoba-tech/kotoba-whisper-v2.0",
+    model_translation="facebook/nllb-200-distilled-600M",
+    tgt_lang="jpn_Jpan",
+    chunk_length_s=15,
+    device_map="auto"
+)
+pipe.push_to_hub("japanese-asr/ja-cascaded-s2t-translation")
